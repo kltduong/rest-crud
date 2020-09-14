3be2901aa1d79ce84fcc54d9bdd315d9e980c3bc
Duong Kieu <kltduong@gmail.com>
2020-09-14 11:59:45 +0000

    Added lambdas and dynamodb

--- lib/rest-crud-stack.ts
+++ lib/rest-crud-stack.ts
@@ -1,5 +1,5 @@
-import dynamodb from '@aws-cdk/aws-dynamodb';
-import lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 import * as path from 'path';
 
@@ -8,13 +8,13 @@ export class RestCrudStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
         const dynamoTable = new dynamodb.Table(this, 'items', {
-      partitionKey: {
-        name: 'itemId',
-        type: dynamodb.AttributeType.STRING
-      },
-      tableName: 'items',
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
-    });
+          partitionKey: {
+            name: 'itemId',
+            type: dynamodb.AttributeType.STRING
+          },
+          tableName: 'items',
+          removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+        });
 
     const getOneLambda = new lambda.Function(this, 'getOneItemFunction', {
       code: new lambda.AssetCode(
